22cdc60a9ff4120aae12a15b416433e5fbd7ae67
Laura-HV <laura.herranzv@edu.uah.es>
2025-02-04 13:23:31 +0100

    tabla productos

---
+++ PL1.sql
@@ -0,0 +1,11 @@
+-- Crear la base de datos PL1
+CREATE DATABASE PL1;
+
+
+-- Crear la tabla productos
+CREATE TABLE productos (
+    producto_id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    stock INT CHECK (stock >= 0),
+    precio DECIMAL(10,2) CHECK (precio >= 10 AND precio <= 5000)
+);
