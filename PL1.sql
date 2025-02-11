0591352470ebfd9f15d6fefb7278b6ea6167bc21
Sergi23 <sco23062005@gmail.com>
2025-02-11 13:06:59 +0100

    diarrea explosiva
    .

--- PL1.sql
+++ PL1.sql
@@ -1,5 +1,7 @@
 
 
+
+
 -- Crear la tabla productos
 CREATE TABLE productos (
     producto_id SERIAL PRIMARY KEY,
@@ -7,3 +9,6 @@ CREATE TABLE productos (
     stock INT CHECK (stock >= 0),
     precio DECIMAL(10,2) CHECK (precio >= 10 AND precio <= 5000)
 );
+
+
+\COPY productos FROM ./productos.csv WITH (FORMAT csv, HEADER, DELIMITER E',', NULL 'NULL', ENCODING 'UTF-8');
