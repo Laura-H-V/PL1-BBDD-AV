31fea85eaea68e5505ac92b08af5e2c169dbe687
Laura-HV <laura.herranzv@edu.uah.es>
2025-02-21 13:29:18 +0100

    odio postgres

--- PL1.sql
+++ PL1.sql
@@ -32,20 +32,21 @@ DROP TABLE productos2temp;
 -- SELECT relpages FROM pg_class WHERE relname = 'productos';
 
 /*
-SELECT blks_read, blks_hit
+SELECT blks_read
 FROM pg_stat_database
 WHERE datname = 'pl1';
 
-SELECT count(*)
-FROM productos
-WHERE  precio = 3000;
-
-
-SELECT blks_read, blks_hit
-FROM pg_stat_database
-WHERE datname = 'pl1';
+SELECT count(*) FROM productos WHERE  precio = 3000;
+SELECT blks_read FROM pg_stat_database WHERE datname = 'pl1';
 
 
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT * FROM productos WHERE precio = 3000;
+
+SELECT COUNT(*) FROM productos WHERE precio BETWEEN 3000.00 AND 3000.99;
+SELECT indexname, indexdef FROM pg_indexes WHERE tablename = 'productos';
+
+SELECT relname, relpages, reltuples, pg_size_pretty(pg_relation_size(oid)) FROM pg_class WHERE relname = 'productos';
 */
+
+
