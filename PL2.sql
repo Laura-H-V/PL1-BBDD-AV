9bf7cdb221d7ec5076b4a3bb53f2ad84d3f6ec3d
Laura-HV <laura.herranzv@edu.uah.es>
2025-03-14 21:59:21 +0100

    diarreic

---
+++ PL2.sql
@@ -0,0 +1,36 @@
+/*CREATE DATABASE investigar;*/
+
+CREATE TABLE investigadores (
+    codigo_investigador NUMERIC PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    apellidos TEXT NOT NULL,
+    salario NUMERIC NOT NULL
+);
+
+CREATE TABLE contratos (
+    codigo_contrato NUMERIC PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    entidad TEXT NOT NULL,
+    coste NUMERIC NOT NULL
+);
+
+CREATE TABLE investigadores_contratos (
+    codigo_investigador NUMERIC,
+    codigo_contrato NUMERIC,
+    horas NUMERIC NOT NULL,
+    PRIMARY KEY (codigo_investigador, codigo_contrato),
+    FOREIGN KEY (codigo_investigador) REFERENCES investigadores(codigo_investigador) 
+        ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY (codigo_contrato) REFERENCES contratos(codigo_contrato) 
+        ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+/*
+\COPY investigadores FROM ./DATOS/datos_investigadores.csv WITH (FORMAT csv, HEADER, DELIMITER E',', NULL 'NULL', ENCODING 'UTF-8');
+\COPY contratos FROM ./DATOS/datos_contratos.csv WITH (FORMAT csv, HEADER, DELIMITER E',', NULL 'NULL', ENCODING 'UTF-8');
+\COPY investigadores_contratos FROM ./DATOS/datos_investigadores_contratos.csv WITH (FORMAT csv, HEADER, DELIMITER E',', NULL 'NULL', ENCODING 'UTF-8');
+*/
+
+SELECT * FROM pg_stats WHERE tablename = 'investigadores';
+SELECT * FROM pg_stats WHERE tablename = 'contratos';
+SELECT * FROM pg_stats WHERE tablename = 'investigadores_contratos';
