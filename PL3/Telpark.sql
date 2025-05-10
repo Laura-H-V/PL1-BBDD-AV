07ffff97457eea11d92a232a89b821af4ae383cd
Sergi23 <sco23062005@gmail.com>
2025-05-10 11:28:10 +0200

    Create Telpark.sql

---
+++ PL3/Telpark.sql
@@ -0,0 +1,75 @@
+SET client_encoding TO 'UTF8';
+
+
+CREATE TABLE clientes (
+    
+    clienteid INTEGER PRIMARY KEY,
+    nombre TEXT,
+    apellido TEXT,
+    telefono INT UNIQUE,
+    email TEXT UNIQUE,
+    provincia TEXT
+
+);
+
+
+CREATE TABLE plazas (
+
+    plazaid INT PRIMARY KEY,
+    numero INT,
+    nivel INT,
+    seccion TEXT
+    
+);
+
+CREATE TABLE vehiculos (
+
+    vehiculoid INT PRIMARY KEY,
+    matricula TEXT UNIQUE,
+    marca TEXT,
+    modelo TEXT,
+    color TEXT,
+    clienteid_clientes INT,
+
+    FOREIGN KEY (clienteid_clientes) REFERENCES clientes(clienteid)
+
+);
+
+CREATE TABLE reservas (
+
+    reservaid INT PRIMARY KEY,
+    fechainicio TIMESTAMP,
+    fechafin TIMESTAMP,
+    vehiculoid_vehiculos INT,
+    plazaid_plazas INT,
+    clienteid_clientes INT,
+
+    FOREIGN KEY (vehiculoid_vehiculos) REFERENCES vehiculos(vehiculoid),
+    FOREIGN KEY (plazaid_plazas) REFERENCES plazas(plazaid),
+    FOREIGN KEY (clienteid_clientes) REFERENCES clientes(clienteid)
+
+);
+
+CREATE TABLE pagos (
+
+    pagoid INT PRIMARY KEY,
+    cantidad NUMERIC(10,2),
+    fechapago TIMESTAMP,
+    metodopago TEXT,
+    reservaid_reservas INT,
+
+    FOREIGN KEY (reservaid_reservas) REFERENCES reservas(reservaid)
+
+);
+
+CREATE TABLE incidencias (
+
+    incidenciaid INTEGER PRIMARY KEY,
+    descripcion TEXT,
+    fechaincidencia TIMESTAMP,
+    estado TEXT,
+    reservaid_reservas INT,
+
+    FOREIGN KEY (reservaid_reservas) REFERENCES reservas(reservaid)  
+
+);
